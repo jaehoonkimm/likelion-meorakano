8c8d5cab0b54a3c6e86e7b1e4516e49da60d8281
Gobans <audghks4452@gmail.com>
2020-05-05 21:02:18 +0900

    update

--- dist/MH_folder/myeonghwan_scroll.js
+++ dist/MH_folder/myeonghwan_scroll.js
@@ -1,12 +1,13 @@
-let all = document.querySelectorAll("button");
+let all = document.querySelectorAll(".scroll_func");
+
 for(let i = 0; all.length; i+= 1){
     if(i%2 == 0){
         all[i].addEventListener('click',function(){
-            window.scrollBy({left:0,top:+700, behavior:'smooth'});
+            window.scrollBy({left:0,top:-650, behavior:'smooth'});
         })
     }else{
         all[i].addEventListener('click',function(){
-            window.scrollBy({left:0,top:-700, behavior:'smooth'});
+            window.scrollBy({left:0,top:+650, behavior:'smooth'});
         })
     };
 };
